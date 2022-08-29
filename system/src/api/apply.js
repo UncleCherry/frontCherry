86ebc4d372a18139af508eba3565a564db2b342c
林晨 <506430249@qq.com>
2022-08-29 11:27:36 +0800

    1

---
+++ system/src/api/apply.js
@@ -0,0 +1,17 @@
+
+// /*
+// 和申请有关的api接口
+// */
+
+// import request from '@/utils/request'
+
+// export function login(data) {
+//     let param = new URLSearchParams(data);
+//     return request({
+//         url: '/Login',
+//         method: 'post',
+//         data:param
+//       })
+// }
+
+
