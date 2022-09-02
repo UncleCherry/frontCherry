afc835c001561e0d4eb3f40da21d23aba275a403
V1ct-0R <913606086@qq.com>
2022-09-02 11:59:53 +0800

    修改教务引导栏

--- system/src/components/AdminNavigate.vue
+++ system/src/components/AdminNavigate.vue
@@ -1,99 +1,100 @@
 <template>
-        <el-container style="height: 100%; border: 1px solid #eee; width:201px; position:fixed">
-  <el-aside width="201px" style="overflow-x: hidden; overflow-y: auto;">
-    <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo side"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b"
-      style="height:100%;text-align: left;">
-      <el-menu-item index="1" @click="OpenEditTrainingPlan">
-        <i class="el-icon-menu" ></i>
-        <span slot="title">编辑专业培养计划</span>
-      </el-menu-item>
-      <el-menu-item index="2"  @click="OpenCreateCourse">
-        <i class="el-icon-menu"></i>
-        <span slot="title">创建课程</span>
-      </el-menu-item>
-      <el-menu-item index="3">
-        <i class="el-icon-menu"></i>
-        <span slot="title">添加考试安排</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-menu"></i>
-        <span slot="title">添加成绩</span>
-      </el-menu-item>
-      <el-submenu index="5" style="text-align: left;">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>申请审核</span>
-        </template>
-          <el-menu-item index="5-1">免修免听申请</el-menu-item>
-          <el-menu-item index="5-2" @click="OpenLeaveVerifyPage">请假申请</el-menu-item>
-      </el-submenu>
-      <el-menu-item index="6"  @click="OpenOnlineCourseManagement">
-        <i class="el-icon-menu"></i>
-        <span slot="title">网课管理</span>
-      </el-menu-item>
-      <el-menu-item index="7" @click="OpenSetting">
-        <i class="el-icon-setting"></i>
-        <span slot="title">设置</span>
-      </el-menu-item>
-    </el-menu>
-   </el-aside>
-    </el-container>
+  <el-container
+    style="height: 100%; border: 1px solid #eee; width: 201px; position: fixed"
+  >
+    <el-aside width="201px" style="overflow-x: hidden; overflow-y: auto">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo side"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        style="height: 100%; text-align: left"
+      >
+        <el-menu-item index="1" @click="OpenEditTrainingPlan">
+          <i class="el-icon-menu"></i>
+          <span slot="title">编辑专业培养计划</span>
+        </el-menu-item>
+        <el-menu-item index="2" @click="OpenCreateCourse">
+          <i class="el-icon-menu"></i>
+          <span slot="title">创建课程</span>
+        </el-menu-item>
+        <el-menu-item index="3" @click="OpenCreatExamPage">
+          <i class="el-icon-menu"></i>
+          <span slot="title">添加考试安排</span>
+        </el-menu-item>
+        <el-menu-item index="4" @click="OpenCreatExamPage">
+          <i class="el-icon-menu"></i>
+          <span slot="title">添加成绩</span>
+        </el-menu-item>
+        <el-menu-item index="5" @click="OpenApplicationVerifyPage">
+          <i class="el-icon-menu"></i>
+          <span slot="title">申请审核</span>
+        </el-menu-item>
+        <el-menu-item index="6" @click="OpenLeaveVerifyPage">
+          <i class="el-icon-menu"></i>
+          <span slot="title">请假申请审核</span>
+        </el-menu-item>
+        <el-menu-item index="7" @click="OpenOnlineCourseManagement">
+          <i class="el-icon-menu"></i>
+          <span slot="title">网课管理</span>
+        </el-menu-item>
+        <el-menu-item index="8" @click="OpenSetting">
+          <i class="el-icon-setting"></i>
+          <span slot="title">设置</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+  </el-container>
 </template>
 <style>
-  .el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 200px;
-    min-height: 400px;
-  }
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
 </style>
 
 <script>
-  export default {
-    data() {
-      return {
-      }
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    OpenCreateCourse() {
+      this.$router.replace("/CreateCourse");
     },
-    methods: {
-      OpenCreateCourse()
-      {
-        this.$router.replace('/CreateCourse');
-      },
-      OpenSetting()
-      {
-        this.$router.replace('/Setting');
-      },
-      OpenOnlineCourseManagement()
-      {
-        this.$router.replace('/OnlineCourseManagement')
-      },
-      OpenEditTrainingPlan()
-      {
-        this.$router.replace('/EditTrainingPlan');
-      },
-      OpenLeaveVerifyPage()
-      {
-        this.$router.replace('/LeaveVerifyPage');
-      }
-    }
-  }
+    OpenSetting() {
+      this.$router.replace("/Setting");
+    },
+    OpenCreatExamPage() {
+      this.$router.replace("/CreatExamPage");
+    },
+    OpenOnlineCourseManagement() {
+      this.$router.replace("/OnlineCourseManagement");
+    },
+    OpenEditTrainingPlan() {
+      this.$router.replace("/EditTrainingPlan");
+    },
+    OpenApplicationVerifyPage() {
+      this.$router.replace("/ApplicationVerifyPage");
+    },
+    OpenLeaveVerifyPage() {
+      this.$router.replace("/LeaveVerifyPage");
+    },
+  },
+};
 </script>
 <style>
 .side {
-  height:700px;
-  width:30%;
-  
+  height: 700px;
+  width: 30%;
 }
 .el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    line-height: 60px;
-  }
-  
-  .el-aside {
-    color: #333;
-  }
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+.el-aside {
+  color: #333;
+}
 </style>
