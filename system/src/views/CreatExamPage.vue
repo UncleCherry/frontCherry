18c36479d2aae5d92a39a6ca180d7950e6c23970
ColdFaceMagicMan <1452045001@qq.com>
2022-09-02 23:45:46 +0800

    typo

--- system/src/views/CreatExamPage.vue
+++ system/src/views/CreatExamPage.vue
@@ -69,28 +69,28 @@
       <div v-if="isCreate">
       <el-row :span="200">
         <el-col :span="20">
-          <el-form :model="ruleForm"  ref="ruleForm" label-width="100px" class="demo-ruleForm" style="width:90%;padding-top:20px;padding-bottom:10px">
+          <el-form :model="changeExamForm"  ref="changeExamForm" label-width="100px" class="demo-ruleForm" style="width:90%;padding-top:20px;padding-bottom:10px">
               <el-form-item label="考试课程" prop="startTime">
-              <el-select v-model="value" placeholder="选择课程" @change="setCourseId">
+              <el-select v-model="value" placeholder="选择课程" @change="setExamId">
               <el-option
-                v-for="item in allCourse"
+                v-for="item in tableData"
                 :key="item.CourseId"
                 :label="item.CourseName"
-                :value="item.CourseId">
+                :value="item.ExamId">
               </el-option>
             </el-select>
             </el-form-item>
           <el-form-item label="开始时间" prop="startTime">
-              <el-input v-model="ruleForm.starttime" placeholder="请输入课程时间  格式:2022-08-08T10:00:00"></el-input>
+              <el-input v-model="changeExamForm.starttime" placeholder="请输入课程时间  格式:2022-08-08T10:00:00"></el-input>
           </el-form-item>
           <el-form-item label="结束时间" prop="endTime">
-              <el-input v-model="ruleForm.endtime" placeholder="请输入课程时间  格式:2022-08-08T11:00:00"></el-input>
+              <el-input v-model="changeExamForm.endtime" placeholder="请输入课程时间  格式:2022-08-08T11:00:00"></el-input>
           </el-form-item>
           <el-form-item label="会议号" prop="meetingId">
-              <el-input v-model="ruleForm.meetingid" placeholder="请输入考试会议号"></el-input>
+              <el-input v-model="changeExamForm.meetingid" placeholder="请输入考试会议号"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="create()">立即创建</el-button>
+            <el-button type="primary" @click="change()">立即修改</el-button>
             <el-button @click="resetForm('ruleForm')">重置</el-button>
           </el-form-item>
           </el-form>
@@ -104,9 +104,6 @@
 
 <script>
 import {getAllExam,changeExam,deleteExam,createExam} from '@/api/Exam.js'
-import CreateExamTable from '@/components/CreateExamTable.vue'
-import ChangeExamTable from '@/components/ChangeExamTable.vue'
-import PlanTable from '@/components/PlanTable.vue'
 import axios from 'axios'
 import { getAllCourse } from '@/api/course'
   export default {
@@ -146,62 +143,35 @@ import { getAllCourse } from '@/api/course'
           courseid: '',
           starttime: '',
           endtime: '',
-          meetingtd: '',
+          meetingid: '',
         },
-        changeForm: {
+        changeExamForm: {
           examid: '',
           starttime: '',
           endtime: '',
-          meetingtd: '',
+          meetingid: '',
         },
 
+        value:"",
         allCourse: [],
         tableData: [],
         dialogTableVisible: false,
         changeExamTableVisible: false,
-        title:'',
-        planData:[],
-        grade:'',
-        major:'',
         isCreate:false,
-        isModify:false,
       }
-    },
-    components:{
-      CreateExamTable
     },
     methods: {
       setCourseId(value){
         this.ruleForm.courseid = value;
         console.log(this.ruleForm.courseid);
       },
-      changeExam(index, row) {
-        this.isModify=true;
-        this.isCreate=false;
-        this.planData=[];
-        this.grade=row["Grade"];
-        this.major=row["Major"];
-        this.title=this.grade+"年"+this.major+"专业培养计划";
-        this.planData.push(eval("(" + row["Info"] + ")"));
-        this.subtotal();
-        this.total();
-        this.dialogTableVisible=true;
-  
-        this.$refs.planTbl.isEdit=false;
-      },
-      modify(){
-        this.$refs.planTbl.isEdit=true;
-        this.$message({
-          message: '单击单元格输入修改',
-          type: 'success'
-        });
+      setExamId(value){
+        this.changeExamForm.examid = value;
       },
       openCreate(){
         this.dialogTableVisible=true;
         this.title="添加考试";
-        this.isModify=false
         this.isCreate=true; 
-        this.$refs.planTbl.isEdit=true;
       },
       create(){
         var param=this.ruleForm;
@@ -222,20 +192,18 @@ import { getAllCourse } from '@/api/course'
       openChange(){
         this.changeExamTableVisible=true;
         this.title="修改考试";
-        this.isModify=false
         this.isCreate=true; 
-        this.$refs.planTbl.isEdit=true;
       },
       change(){
-        var param=this.ruleForm;
+        var param=this.changeExamForm;
         axios.all([changeExam(param).then(response=>{
           this.$message({
-            message: '创建考试成功',
+            message: '修改考试成功',
             type: 'success'
           });
         }).catch((error)=>{
           this.$message({
-            message: '创建考试失败',
+            message: '修改考试失败',
             type: 'warning'
           });
         })]).then(()=>{
